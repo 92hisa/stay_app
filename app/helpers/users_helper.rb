482e34e071ad13dcefd4b1cf4d0db86e9335225c
kunihisa oda <kunihisa.oda@gmail.com>
2021-03-03 08:14:04 +0000

    for PR

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-  
-def text_placeholder
-  <<-"EOS".strip_heredoc
-    （記入例）
-    福岡市天神にあるセカンドハウスを１回２泊以上よりお貸しします。
-  EOS
-end
-  
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -39,6 +39,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [300, 300]
   end
 
+
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
